cf48207c94452afca3c8c06ca3f590e082e9a43d
gchubarich <gchubarich@gmail.com>
2017-07-28 16:09:53 +0300

    Add some fixes for testing

--- app/src/main/java/ru/karapetiandav/yamblzproject/ui/cities/presenter/CitiesPresenter.java
+++ app/src/main/java/ru/karapetiandav/yamblzproject/ui/cities/presenter/CitiesPresenter.java
@@ -1,11 +1,13 @@
 package ru.karapetiandav.yamblzproject.ui.cities.presenter;
 
 
+import android.support.annotation.NonNull;
+
 import io.reactivex.Observable;
 import ru.karapetiandav.yamblzproject.ui.base.presenter.Presenter;
 import ru.karapetiandav.yamblzproject.ui.cities.model.CityViewModel;
 
 public interface CitiesPresenter<V> extends Presenter<V> {
-    void onCityClick(CityViewModel city);
+    void onCityClick(@NonNull CityViewModel city);
     void observeInputChanges(Observable<CharSequence> inputChanges);
 }
